6efd503efb3af08f672d534b5cd30c4438f0709c
Yogesh Singh Bisht <85386911+yogeshbisht01@users.noreply.github.com>
2022-06-16 17:05:56 +0530

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from tkinter.filedialog import askopenfile
-headers = {"Authorization": "Bearer ya29.A0ARrdaM97g-mtUySZ8FKLIpd0LXKWfcJOLuEnYZk_k66_nVvb4h6GzAiTUptvQDKOvK7XVdH9LoQmgAl_cn3alb9mOmLROZwlzKA4GVQyeVXURXqkU47EywW2UEmeN9Thb_12EZA7qxTkpVZMNf7j1_Pd41sY"}
+headers = {"Authorization": "Bearer ya29.A0ARrdaM97g-mtUySZ8FKLIpd0LXKWfcJOLuEnYZk_k66_nVvb4h6GzAiTUptvQDKOvK7XVdH9LoQmgAl_cn3alb9mOmLROZwlzKA4GVQyeVXURXqkU47EywW2UEmeN9Thb_12EZA7qxTkpVZMNf7j1_Pd41sY"} #add your oath authorization key, of the mail you want to use for your note backup
 para = {
     "name": "Pynote.txt",
 }
